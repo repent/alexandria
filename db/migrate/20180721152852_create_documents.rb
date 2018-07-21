4e31cdf308579f1aed3a69c07d6bcb5b6f54c995
Dan Hetherington <dan.hetherington@gmail.com>
2018-07-21 16:29:08 +0100

    Documents scaffold

---
+++ db/migrate/20180721152852_create_documents.rb
@@ -0,0 +1,9 @@
+class CreateDocuments < ActiveRecord::Migration[5.1]
+  def change
+    create_table :documents do |t|
+      t.belongs_to :owner, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
